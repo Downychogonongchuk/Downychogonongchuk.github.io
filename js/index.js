9a7549982ec7f4a29729a7971941dee7a43fb919
Civilian <renounce774@naver.com>
2024-01-23 16:07:01 +0900

    const

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 const fun1 = ()=>{
     alert("hello");
 }
+// 텍스트 요소
+const $text = document.querySelector(".text");
+// 입력될 글자 모음
+const letters = ["HTML", "CSS", "JavaScript"];
+// 글자 입력 속도
+const speed = 100;
+// 현재 지정된 글자
+let i = 0;
+
